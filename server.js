fdb9d67ad3b1cac194334d5c21a526f1ff150202
LAgrl <lizethariasc14@gmail.com>
2021-07-04 11:59:26 -0700

    added Init function

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const mysql = require('mysql2');
-const express = require('express');
-
-const PORT = process.env.PORT || 3002;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
 
 // Connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
+      port: 3002,
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
@@ -21,14 +16,59 @@ const db = mysql.createConnection(
     console.log('Connected to the employees database.')
   );
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
+// Create Connection
+connection.connect(function(err) {
+    if (err) throw err;
+    Init();
+});
 
+function Init() {
+    inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all employees",
+        "View all employees by department",
+        "View all employees by role",
+        "Add employee",
+        "Add department",
+        "Add role",
+        "Update employee role"
+      ]
+    })
+    .then(function(answer) {
+        switch (answer.action) {
+            case "View all employees":
+                viewAllEmp();
+                break;
 
+            case "View all employees by department":
+                viewAllEmpByDept();
+                break;
 
+            case "View all employees by role":
+                viewAllEmpByRole();
+                break;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+            case "Add employee":
+                addEmp();
+                break;
+
+            case "Add department":
+                addDept();
+                break;
+            case "Add role":
+                songAndAlbumSearch();
+                break;
+            case "Update employee role":
+                updateEmpRole();
+                break;
+        }
+    });
+}
+
+function viewAllEmp(){
+    Init();
+}
