1710bdfea8d3c0a2a98646bd17aba66f5784683f
LAgrl <lizethariasc14@gmail.com>
2021-07-04 12:42:42 -0700

    view all employees function

--- server.js
+++ server.js
@@ -45,10 +45,18 @@ function Init() {
         "Add employee",
         "Add department",
         "Add role",
-        "Update employee role"
+        "Update employee role",
+        "Update employee manager",
+        "Delete employee",
+        "Delete role",
+        "Delete department",
+        "View department budgets"
       ]
     })
     .then(function(answer) {
+
+
+       // Switch case depending on user option
         switch (answer.action) {
             case "View all employees":
                 viewAllEmp();
@@ -70,15 +78,48 @@ function Init() {
                 addDept();
                 break;
             case "Add role":
-                songAndAlbumSearch();
+                addRole();
                 break;
             case "Update employee role":
                 updateEmpRole();
+                break;
+            case "Update employee manager":
+                updateEmpMngr();
+                break;
+            case "View all employees by manager":
+                viewAllEmpByMngr();
+                break;
+            case "Delete employee":
+                deleteEmp();
+                break;
+            case "View department budgets":
+                viewDeptBudget();
+                break;
+            case "Delete role":
+                deleteRole();
+                break;
+            case "Delete department":
+                deleteDept();
                 break;
         }
     });
 }
 
+// View all employees 
 function viewAllEmp(){
-    Init();
+
+    // Query to view all employees
+    let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
+
+    // Query from connection
+    connection.query(query, function(err, res) {
+        if(err) return err;
+        console.log("\n");
+
+        // Display query results using console.table
+        console.table(res);
+
+        //Back to start
+        Init();
+    });
 }
