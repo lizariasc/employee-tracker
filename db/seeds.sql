f0d4a48691f758a70fca4eb007a343d3d675d5bd
LAgrl <lizethariasc14@gmail.com>
2021-07-04 11:14:16 -0700

    populate employee table with seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) 
+VALUES
+(4, "Charbelle", "Chaz", 3, null),
+(5, "Sofi", "Hernan", 4, null),
+(7, "Char", "Dant", 6, null),
+(10, "Jonathan", "Uong", 8, null),
+(3, "Melissa", "Arm", 2, 4),
+(1, "Alla", "Arous", 1, 3),
+(2, "Rosie", "Cole", 1, 3),
+(6, "Lianne", "Soon", 5, 7),
+(8, "Zach", "Soar", 7, 10),
+(9, "Martin", "Cast", 8, 10);
